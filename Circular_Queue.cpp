2fc3cd16484482318efc7e8fda731964c295dbe8
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-02-12 13:33:11 +0530

    Create Circular_Queue.cpp

---
+++ Circular_Queue.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+
+class CirQueue{
+        public:
+        int size;
+        int *arr;
+        int front;
+        int rear;
+
+        CirQueue(int size) {
+                this->size = size;
+                arr = new int[size];
+                front = -1;
+                rear = -1;
+        }
+
+        void push(int data) {
+                //Queue Full
+
+                //single element case - > first element
+
+                //circular nature
+
+                //normal flow
+                //TODO: add one more condition in the QUEUE FULL if block
+                if((front == 0 && rear == size-1)) {
+                        cout << "Q is fulll, cannot insert" << endl;
+                }
+                else if(front == -1) {
+                        front = rear = 0;
+                        arr[rear] = data;
+                }
+                else if(rear == size-1 && front != 0 ) {
+                        rear = 0;
+                        arr[rear] = data;
+                }
+                else{
+                        rear++;
+                        arr[rear]= data;
+                } 
+        }
+
+        void pop() {
+                //empty check
+                //single element
+                //circular nature
+                //normal flow
+                if(front == -1) {
+                        cout << "Q is empty , cannot pop" << endl;
+                }
+                else if (front == rear) {
+                        arr[front] = -1;
+                        front = -1;
+                        rear = -1;  
+                }
+                else if(front == size-1) {
+                        front = 0;
+                }
+                else {
+                        front++;
+                }
+        }
+
+};
+
+int main() {
+    int size;
+    cout << "Enter the size of the Circular Queue: ";
+    cin >> size;
+
+    CirQueue cq(size);
+
+    cq.push(1);
+    cq.push(2);
+    cq.push(3);
+
+    cq.pop();
+    cq.pop();
+    cq.pop();
+    cq.pop();
+
+    return 0;
+}
